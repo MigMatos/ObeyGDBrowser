2a1fba5fa07cfd17737c5c2d6caf080e24504a78
migmatos <matosmoo2002@gmail.com>
2024-05-04 23:32:09 -0600

    0.251

--- update/index.php
+++ update/index.php
@@ -11,7 +11,7 @@ if ($isAdmin != "1" || $logged != true) {
     $source = "../ogbrowser_init_updater.php";
     $destination = "../../ogbrowser_init_updater.php";
 
-    if (file_exists($source)) {
+    
         if (!rename($source, $destination)) {
             if (file_exists($destination) && is_writable($destination)) {
                 unlink($destination);
@@ -19,7 +19,7 @@ if ($isAdmin != "1" || $logged != true) {
                 unlink($source);
             }
         }
-    }
+    
     
     header("Location: ../../ogbrowser_init_updater.php");
     exit();
